0614ef62a82b5a85fda6b5c5873b286e72d6dca1
TomDespres <tom.despres@mmibordeaux.com>
2020-03-19 18:45:41 +0100

    ajout form img dans ajoutprojoPHP

--- ajoutProjet.php
+++ ajoutProjet.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
+
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title></title>
   </head>
   <body>
-
+    <form enctype="multipart/form-data" action="_URL_" method="post">
+      <!-- MAX_FILE_SIZE doit précéder le champ input de type file -->
+      <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
+      <!-- Le nom de l'élément input détermine le nom dans le tableau $_FILES -->
+      Envoyez ce fichier : <input name="userfile" type="file" />
+      <input type="submit" value="Envoyer le fichier" />
+    </form>
     <form action=".php" method="post">
 
     <div class="container">
